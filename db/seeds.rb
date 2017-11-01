73452ed09f66718e24f65b868e29ed7a41cda544
Philippe Ka <ruby@knafo.fr>
2017-11-01 17:09:42 +0100

    exo ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+category = %w(chinese italian japanese french belgian)
+
+
+puts "Deleting all Restaurants"
+Restaurant.destroy_all
+puts "Creating 50 restaurants"
+50.times do
+  restaurant = Restaurant.create!(name: Faker::Cat.name, address: "#{Faker::Address.street_address} #{Faker::Address.zip_code} #{Faker::Address.city}", category: category.sample)
+  puts "Created #{restaurant.name}"
+end
+
+puts "Bye!"
